9768ff045d2cffbcb1567dc645628dbb931cde9a
JonnyFernandez <arcancode@gmail.com>
2025-10-05 11:32:30 -0300

    first commit02

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,67 +1,35 @@
-import AuthProvider from './context/AuthContext';
-import ProdProvider from './context/ProdContext';
-import {
-  Register, Login, Home, Calculator, Butget, SalesReport, StockReport,
-  Statistics, OrderDetail, AddProduct, Category, Distributor, Catalog,
-  UpdateStock, ProdDetail, EditProd, UserManagement, Distributor_Category,
-  LocalSale, OrderUpdate, ReportDetails
-} from './pages/index';
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
-import ProtectedRoute from "./ProtectedRoutes";
-import { NavBar } from './components';
-import './index.css'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-function AppContent() {
-  const location = useLocation();
-  const hideNavRoutes = ["/", "/register"];
-  const shouldHideNav = hideNavRoutes.includes(location.pathname);
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      {!shouldHideNav && <NavBar />}
-
-      <main>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-
-          <Route element={<ProtectedRoute />}>
-            <Route path="/home" element={<LocalSale />} />
-            <Route path="/calculator" element={<Calculator />} />
-            <Route path="/butget" element={<Butget />} />
-            <Route path="/prod" element={<AddProduct />} />
-            <Route path="/sales-report" element={<SalesReport />} />
-            <Route path="/stock-report" element={<StockReport />} />
-            <Route path="/statistics-report" element={<Statistics />} />
-            <Route path="/order-detail/:id" element={<OrderDetail />} />
-            <Route path="/order-update/:id" element={<OrderUpdate />} />
-
-            <Route path="/reportDetails" element={<ReportDetails />} />
-            <Route path="/category" element={<Category />} />
-            <Route path="/distributor" element={<Distributor />} />
-            <Route path="/catalog" element={<Catalog />} />
-            <Route path="/update-stock" element={<UpdateStock />} />
-            <Route path="/detail/:id" element={<ProdDetail />} />
-            <Route path="/prod-edit/:id" element={<EditProd />} />
-            <Route path="/users" element={<UserManagement />} />
-            <Route path="/config/00649f28-94dd-412b-8c70-7687dc2fa087" element={<Distributor_Category />} />
-          </Route>
-        </Routes>
-      </main>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
     </>
-  );
+  )
 }
 
-function App() {
-  return (
-    <AuthProvider>
-      <ProdProvider>
-        <BrowserRouter>
-          <AppContent />
-        </BrowserRouter>
-      </ProdProvider>
-    </AuthProvider>
-  );
-}
-
-export default App;
+export default App
--- client/src/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-    baseURL: 'http://localhost:3001/api',
-    // baseURL: 'https://port-control-api-production.up.railway.app/api',
-})
-
-export default instance
